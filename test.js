01ab1511301ce14112ed2b2afa8e2b08c3e9ac1e
Max Base <MaxBaseCode@Gmail.Com>
2023-04-08 20:32:25 +0100

    clean code: test.js

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 const create_rss = require("./create-rss");
 
-const rss = new create_rss;
+// Create a new RSS feed
+const rss = new create_rss();
+
+// Set the channel options
 rss.setChannel("MY", "https://www.google.com", "This is a test", "en");
+
+// Add a few test items
+rss.addItem("Hello, World", "https://www.google.com/hello-world", "This is a hello-world test!");
+rss.addItem("Hello, World 2", "https://www.google.com/hello-world2", "This is a hello-world test!");
+rss.addItem("Hello, World 3", "https://www.google.com/hello-world3", "This is a hello-world test!");
+
+// Generate the RSS feed
 console.log(rss.generate());
